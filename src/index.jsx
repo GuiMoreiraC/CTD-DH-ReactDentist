004f533f14e83ac0e42dfc894634897f03747913
silverirac <caueasilveira@gmail.com>
2023-06-26 23:23:18 -0300

    feat: implementação de rotas

--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,31 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import Navbar from "./Components/Navbar";
 import Home from "./Routes/Home";
+import Login from "./Routes/Login";
+import Detail from "./Routes/Detail";
 import Footer from "./Components/Footer";
+import App from "./App"
 import "./index.css";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //Lembre-se de configurar suas rotas e seu contexto aqui
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/home", element: <Navigate to={"/"} /> },
+      { path: "/dentist/:id", element: <Detail /> },
+      { path: "/login", element: <Login /> }
+    ]
+    
+  },
+  
+])
+
 root.render(
-  <React.StrictMode>
-    <Navbar />
-    <Home />
-    <Footer />
-  </React.StrictMode>
+  <RouterProvider router={router}/>
 );
